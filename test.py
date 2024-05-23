3bd2cb3d5bedd12f321187ea6fed0cb6dfe800af
DimaLab <98299576+LomakoDima@users.noreply.github.com>
2024-05-23 21:34:29 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
+import random
 
-word = input("Введите непонятное слово (большими буквами!): ")
-
-if word in meme_dict.keys():
-    print(name_dict[word])
-else:
-    print("no word")
+chars = '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+number = input('количество паролей?'+ "\n")
+length = input('длина пароля?'+ "\n")
+number = int(number)
+length = int(length)
+for n in range(number):
+    password =''
+    for i in range(length):
+        password += random.choice(chars)
+    print(password)
